52bc3390d312af0e6d42fc99b30a57006a6b568f
Autumnorliu <18708311791@163.com>
2020-09-01 11:52:29 +0800

    添加了rango应用的urls.py，使其能处理rango开头的网页

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-from rango import views
+from django import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),   # 把rango开头的url交给rango app处理
 ]
