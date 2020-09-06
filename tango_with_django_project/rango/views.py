dfaa3afa696cb0922dd65eb5c11ad4214c56f3c3
Autumnorliu <18708311791@163.com>
2020-09-06 09:32:00 +0800

    新增了首页显示前五分类模块，并添加了category.html

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 
 def index(request):
-    content_text = {'boldmessage': 'This message is depend on you!'}
-    return render(request, 'rango/index.html', context=content_text)
+    category_list = Category.objects.order_by('-likes')[:5]
+    content_text = {'categories': category_list}
+    return render(request, 'rango/index.html', content_text)
 
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
